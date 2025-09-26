3c4bb305d5f40b91b0ab8648e09c9c0b24bcfd73
Jonathan K-N <jkakesa9@gmail.com>
2025-09-26 05:27:54 -0400

    fix son buzzer

---
+++ static/buzzer.js
@@ -0,0 +1,22 @@
+// Fonction pour générer un son de buzzer
+function playBuzzerSound() {
+    try {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const oscillator = audioContext.createOscillator();
+        const gainNode = audioContext.createGain();
+        
+        oscillator.connect(gainNode);
+        gainNode.connect(audioContext.destination);
+        
+        oscillator.frequency.setValueAtTime(800, audioContext.currentTime);
+        oscillator.type = 'square';
+        
+        gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
+        gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.5);
+        
+        oscillator.start(audioContext.currentTime);
+        oscillator.stop(audioContext.currentTime + 0.5);
+    } catch(err) {
+        console.log('Son non disponible');
+    }
+}
